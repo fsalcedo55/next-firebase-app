19763beb2d6e6ec69f45e7a77b89a4a2c347d9f6
fsalcedo55 <fsalcedo55@hotmail.com>
2022-08-20 02:52:42 -0400

    Add new auth changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,36 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
+import { UserContext } from "../lib/context";
+import { useState, useEffect, useContext } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { addDoc, setDoc, doc, collection } from "firebase/firestore";
 
 function MyApp({ Component, pageProps }) {
+  const auth = getAuth();
+  const collectionRef = collection(firestore, "users");
+  const documentRef = doc(firestore, "users");
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    let unsubscribe;
+    onAuthStateChanged(
+      auth,
+      (user) => {
+        if (user) {
+          const ref = firestore.collection("users").doc(user.uid);
+        }
+      },
+      []
+    );
+  });
+
   return (
-    <>
+    <UserContext.Provider value={{ user: {}, username: "fernando" }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   );
 }
 
--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,60 @@
-export default function EnterPage() {
+import { initializeApp } from "../lib/firebase";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+
+const googleAuthProvider = new GoogleAuthProvider();
+const auth = getAuth();
+
+export default function EnterPage(props) {
+  const [user, username] = useContext(UserContext);
   return (
     <main>
-      <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UserNameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 }
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, googleAuthProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} />
+      Sign In With Google
+    </button>
+  );
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UserNameForm() {}
